77eceab29fd2da4dd1cd2cf9363e08f949ea2000
Joan Barba <joanbarba19@gmail.com>
2016-10-13 12:36:39 +0200

    adding test

---
+++ test.py
@@ -0,0 +1,17 @@
+from poker import card_ranks, kind
+
+def test():
+    "Test cases for the functions in poker program."
+    sf = "6C 7C 8C 9C TC".split() # Straight Flush
+    fk = "9D 9H 9S 9C 7D".split() # Four of a Kind
+    fh = "TD TC TH 7C 7D".split() # Full House
+    tp = "TD 9H TH 7C 3S".split() # Two Pair
+    fkranks = card_ranks(fk)
+    tpranks = card_ranks(tp)
+    assert kind(4, fkranks) == 9
+    assert kind(3, fkranks) == None
+    assert kind(2, fkranks) == None
+    assert kind(1, fkranks) == 7
+    return 'tests pass'
+
+print test()
