07ac385df8d64a106ee22ae96a2e2a08979e25c8
Joan Barba <joanbarba19@gmail.com>
2016-10-13 11:53:17 +0200

    Random hands

--- poker.py
+++ poker.py
@@ -1,3 +1,9 @@
+import random
+
+def deal(numhands, n=5, deck=[r+s for r in '23456789TJQKA' for s in 'SHDC']):
+    "Return n random hands"
+    random.shuffle(deck)
+    return [deck[n*i:n*(i+1)] for i in range(numhands)]
 
 def poker(hands):
     return allmax(hands, key = hand_rank)
